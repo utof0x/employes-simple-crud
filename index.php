bd26ecf1b97615b74444fb52334ee7aa2d68490c
Mykhailo Popovych <popovychmykhailo00@gmail.com>
2023-07-01 23:54:41 +0300

    created basic layout

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <title>Employee simple CRUD</title>
+</head>
+<body>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>First Name</th>
+        <th>Second Name</th>
+        <th>Title</th>
+        <th>Phone number</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Mock First Name</td>
+        <td>Mock Second Name</td>
+        <td>Mock Title</td>
+        <td>Mock Phone Number</td>
+        <td>Mock Email</td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
