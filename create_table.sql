d4e529830373380563b2b40d458c077bfd16ddc8
Mykhailo Popovych <popovychmykhailo00@gmail.com>
2023-07-01 23:55:07 +0300

    created basic table

---
+++ create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `sql_employee`.`employee` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(255) NOT NULL,
+  `last_name` VARCHAR(255) NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `phone_number` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`));
